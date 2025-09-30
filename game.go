27eab8b120b9e52d3dc155efaab7e8a7effc2d3e
CJMeek <charliejmeek@gmail.com>
2025-09-30 13:50:14 +0100

    rambling comments relaying my thoughts onto paper to transfer between machines

--- game.go
+++ game.go
@@ -14,10 +14,23 @@ type Game struct {
 
 func (g *Game) Run(quit <-chan struct{}) {
 
+	//painted map to show where trails are as to avoid collisions
+
+	// make players move constantly
+	// change keys purely to change direction
+
+	//if direction = up, or down then y-- or y++
+	//if direciton = left or right then x-- or x++
+	//may be an edge case where player suddenly moves diagonally
+	//edge case where players move into the same space at the same time or die at the same time -> need to add flag for both players crashing every tick
+
+	//create a draw function that draws the players and their trails
+	//fills in gaps on lower framerates -> seeing issue with 30fps (33ms ticker). Could be a problem for online multiplayer
+
 	playerStyle := tcell.StyleDefault.Background(tcell.ColorWhite).Foreground(tcell.ColorWhite)
 	trailStyle1 := tcell.StyleDefault.Background(g.Player1.Colour).Foreground(g.Player1.Colour)
 	trailStyle2 := tcell.StyleDefault.Background(g.Player2.Colour).Foreground(g.Player2.Colour)
-	ticker := time.NewTicker(33 * time.Millisecond)
+	ticker := time.NewTicker(16 * time.Millisecond)
 	defer ticker.Stop()
 
 	lastX1, lastY1 := g.Player1.X, g.Player1.Y
